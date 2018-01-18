0d9d89ddf66523b47e3065f8f16120dc1575cfe4
Justin <booljayj@gmail.com>
2018-01-18 20:56:26 +0100

    Improve main text box
    - Use the same panel for both text and options
    - Switch to direct calling method on story buttons
    - Implement button functions directly in story button behavior
    - Modify masking and parenting in main panel for better flow

--- Assets/Black Ring/Scripts/StoryButton.cs
+++ Assets/Black Ring/Scripts/StoryButton.cs
@@ -1,17 +1,62 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using TMPro;
 
-public class StoryButton : MonoBehaviour {
-	[SerializeField] Button buttonComponent;
+public class StoryButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler {
 	[SerializeField] TMP_Text textComponent;
+	[SerializeField] Image imageComponent;
+
+	[Header( "Normal" )]
+	[SerializeField] Color textColor = Color.black;
+	[SerializeField] Color backgroundColor = Color.clear;
+
+	[Header( "Hover" )]
+	[SerializeField] Color textHoverColor = Color.white;
+	[SerializeField] Color backgroundHoverColor = Color.black;
+
+	StoryReading.StoryReader reader;
+	int optionIndex;
 
 	public string text {
 		get{ return textComponent.text; }
 		set{ textComponent.text = value; }
 	}
 
-	public Button.ButtonClickedEvent onClick {
-		get{ return buttonComponent.onClick; }
+	public void BindOption( StoryReading.StoryReader newReader, int newOptionIndex ) {
+		reader = newReader;
+		optionIndex = newOptionIndex;
 	}
+
+	public void OnEnable() {
+        textComponent.color = textColor;
+		imageComponent.color = backgroundColor;
+	}
+
+	public void OnValidate() {
+		if( textComponent ) {
+			textComponent.text = gameObject.name;
+			textComponent.color = textColor;
+		}
+		if( imageComponent ) {
+			imageComponent.color = backgroundColor;
+		}
+	}
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+		textComponent.color = textHoverColor;
+		imageComponent.color = backgroundHoverColor;
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        textComponent.color = textColor;
+		imageComponent.color = backgroundColor;
+    }
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+		reader.ChooseOption( optionIndex );
+    }
 }
